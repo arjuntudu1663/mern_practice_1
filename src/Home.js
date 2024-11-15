c319ad71db0eac6b4b36fa8dc68f0143614d38eb
arjuntudu <arjuntudu9163@gmail.com>
2024-11-16 02:05:20 +0530

    dawdwadwa

--- src/Home.js
+++ src/Home.js
@@ -2,22 +2,44 @@ import React from 'react'
 import { useState,useEffect } from 'react';
 import { color, motion } from 'framer-motion'
 import { BiAlignLeft  } from "react-icons/bi";
-import { Row,Col } from 'react-bootstrap';
+import { Row,Col, Button } from 'react-bootstrap';
 import  axios  from 'axios';
 
 
 
 
 
-const Home = () => {
+const Home = () => { 
+
+  const [value,setValue] = useState(1)
+
+   const show = function(value) {
+     
+       if(value == 'p'){
+         setValue((prev)=>{
+          return prev+0.1
+         })
+       }else{
+          setValue((prev)=>{
+            return prev-0.1
+          })
+       }
+
+   }
 
   
     useEffect(()=>{
      
-        const getValue = function(){ 
+        const getValue = async function(){ 
              
-            const response = axios.get("http://localhost:5000/first_api");
-            console.log(response)
+            try{
+              const response = axios.get("http://localhost:5000/first_api");
+              console.log(response)
+            }catch(err){
+               if(err){
+                 console.log("api receiving failed")
+               }
+            }
             
     
         }
@@ -32,7 +54,15 @@ const Home = () => {
       
       <h1 style={{color:"white",marginTop:"15px"}} >Okaare.in</h1>
       
-      <motion.div className='myshadow' style={{marginTop:"15%",padding:"20px", borderRadius:"15px",height:"100px",width:"390px",backgroundColor:"red"}} initial = {{scale:0.5}} animate = {{scale:1}} >
+      <motion.div
+      
+      
+       
+      className='myshadow' 
+      style={{marginTop:"15%",padding:"20px", borderRadius:"15px",height:"100px",width:"390px",backgroundColor:"red"}} 
+      initial = {{scale:0.5}} 
+      animate = {{scale:value}} >
+      
          
          <Row>
              <Col sm = {6} >
@@ -44,7 +74,13 @@ const Home = () => {
          </Row>
     
       </motion.div>
+      <div style={{height:"100px"}} >
 
+      </div>
+      
+      <Button onClick={e=>show("p")} style={{width:"33%",marginTop:"50px",height:"50px",borderRadius:"15px"}} variant="primary">increase</Button>
+      <br/>
+      <Button onClick={e=>show("n")} style={{width:"33%",marginTop:"50px",height:"50px",borderRadius:"15px"}} variant="primary">decrease</Button>
 
     </div>
   )
