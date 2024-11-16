f25aa72f10fd2691e8a2dd2d1bc4f1f334c5ea89
arjuntudu <arjuntudu9163@gmail.com>
2024-11-16 11:22:02 +0530

    dfawdaw

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState,useEffect } from 'react';
 import { color, motion } from 'framer-motion'
 import { CiLocationArrow1 , CiTextAlignJustify ,CiBookmark  } from "react-icons/ci";
-import { Row,Col, Button ,Image } from 'react-bootstrap';
+import { Row,Col, Button ,Image,Form  } from 'react-bootstrap';
 import  axios  from 'axios';
 
 
@@ -14,66 +14,33 @@ const Home = () => {
   const [value,setValue] = useState(1);
   const [slide,setSlide] = useState(0);
 
-  const makeslide = function(value){ 
+  const [posts,setPosts] = useState([]);
 
-       if(value == 'r'){
-         
-          setSlide((prev)=>{
-             
-            return prev+50
+  const [post,setPost] = useState({
+     name:"",
+     value:"",
+     date : new Date()
+  })
 
-          })
 
-       }else{
-           
-          setSlide((prev)=>{
-             
-            return prev-50
-
-          })
-        
-
-       }
+  const addPost = () => {
      
-     
+    setPosts((prev)=>{
+       return [...prev,post]
+    })
+    setPost((prev)=>{
+      return {...prev,name:"",value:""}
+    })
+
   }
 
-   const show = function(value) {
-     
-       if(value == 'p'){
-         setValue((prev)=>{
-          return prev+50
-         })
-       }else{
-          setValue((prev)=>{
-            return prev-50
-          })
-       }
+  
 
-   }
-
+   
   
     useEffect(()=>{
      
-        const getValue  = async () => { 
-             
-            try{
-              
-              const response = axios.get("http://localhost:5000/first_api");
-
-              console.log(response.data)
-
-
-            }catch(err){
-               if(err){
-                 console.log("api receiving failed")
-               }
-            }
-            
-    
-        }
-
-        getValue();
+       
         
     
     },[])
@@ -82,14 +49,14 @@ const Home = () => {
 
     
       
-      <>
+      <div style={{padding:"15px"}}>
       <p></p>
-      <div style={{width:"100%",marginTop:"15px",display:"flex",justifyContent:"center"}} className='myCenterBox'  >
+      <div style={{marginTop:"15px",display:"flex",justifyContent:"center"}} className='myCenterBox'  >
          
           <p></p>
           
           
-          <div style={{backgroundColor:"black",marginTop:"1px",borderRadius:"15px",paddingLeft:"15px",paddingRight:"15px",height:"80px",display:"flex",alignItems:"center",justifyContent:"space-between"}} className='mybox'>
+          <div style={{backgroundColor:"black",marginTop:"1px",borderRadius:"15px",height:"80px",display:"flex",padding:"15px",alignItems:"center",justifyContent:"space-between"}} className='mybox'>
             
              <h1 className='header_font' style={{color:"white"}} >
               Okaare.In
@@ -106,26 +73,54 @@ const Home = () => {
       </div>
        <p></p>
 
-       <div style={{backgroundColor:"",height:"50px",marginTop:"15px",padding:"15px"}} className='myCenterBox'>
+       <div style={{height:"50px",marginTop:"15px"}} className='myCenterBox'>
          
                 <Row>
-                  <Col style={{backgroundColor:"",height:"auto"}} lg = {6} sm = {12} >
-                      <h1 className='desc_font' >This is how we did</h1>
+                  <Col style={{backgroundColor:"",marginBottom:"15px",height:"auto",display:"flex",flexDirection:"column",justifyContent:"space-between"}} lg = {6} sm = {12} >
+                     <img style={{width:"100%",borderRadius:"15px"}} src={require("./pic.jpg")} />
+                    
                   </Col>
                   <Col style={{height:"auto"}} lg = {6} sm = {12} >
                       
-                      <img style={{width:"100%",borderRadius:"15px"}} src={require("./pic.jpg")} />
+                      
+                      <h1 className='desc_font' >This is how we did</h1>
+                      <Form>
+                     
+                        
+                        <Form.Control value={post.name} onChange={e=>setPost((prev)=>{
+                          return {...prev,name:e.target.value}
+                        })}  placeholder='name' rows={3} />
+                    
+                      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                        <br></br>
+                        <Form.Control placeholder='type your post ....' value = {post.value} onChange={e=>setPost((prev)=>{
+                          return {...prev,value:e.target.value}
+                        })} as="textarea" rows={3} />
+                      </Form.Group>
+                    </Form>
+                      <Button onClick={addPost} variant='dark' style={{borderRadius:"15px"}} >make a post</Button>
 
                   </Col>
 
 
                 </Row>
+                <hr></hr>
+                {
+                  posts.map((x)=>{
+                    return <div style={{width:"100%",marginBottom:"15px",backgroundColor:"black",padding:"15px",borderRadius:"15px"}} className='myShadow' >
+                      <h4 style={{color:"white"}}>{x.name}</h4>
+                      <h1 style={{color:"white"}} >
+                        
+                        {x.value}</h1>
+                        <p>{x.date}</p>
+                    </div>
+                  })
+                }
          
        </div>
 
 
-      </>
-
+      </div>
 
                
           
