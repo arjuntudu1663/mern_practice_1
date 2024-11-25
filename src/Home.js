63e2c9abaaf7e25e90ec657b68acfa776955a3ce
arjuntudu <arjuntudu9163@gmail.com>
2024-11-25 11:23:24 +0530

    dawdawdawdw

--- src/Home.js
+++ src/Home.js
@@ -91,7 +91,7 @@ const Home = () => {
             
             console.log(response," <========= getData response");
             setPosts(response.data);
-            window.location.reload();
+            
 
             
 
